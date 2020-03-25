e040e17543d72850676edffdd3a0cfc4d19ebe2a
Marcelo Paixão Resende <marcelothebuilder@users.noreply.github.com>
2020-03-25 04:19:08 -0300

    configure logging

--- runner.py
+++ runner.py
@@ -4,7 +4,9 @@ from scrapy.utils.project import get_project_settings
 from dotenv import load_dotenv
 from olxscraper.spiders.Olx import OlxSpider
 import logging
+import os
 
+logging.basicConfig(filename='olx-monitor.log', level=os.getenv("DEFAULT_LOGGING_LEVEL", logging.INFO))
 logging.getLogger('scrapy_deltafetch.middleware').setLevel(logging.WARN)
 
 # OR, the same with increased verbosity
