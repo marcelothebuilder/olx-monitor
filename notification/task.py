b81877c82629a7c6085287dcc1d9705177fd4da4
Marcelo Paixão Resende <marcelothebuilder@users.noreply.github.com>
2020-03-26 03:09:28 -0300

    try-catch at telegram notification

--- notification/task.py
+++ notification/task.py
@@ -1,15 +1,21 @@
 from database import Product, session
 from notification.telegram_bot import send_notification
 
+import logging
 
 def send_notifications():
     new_products = Product.get_new()
 
     for product in new_products:
         p: Product = product
-        send_notification(p.image, p.title, p.description, p.price, p.city, p.region, p.url)
-        p.is_new = False
-        session.commit()
+        try:
+            send_notification(p.image, p.title, p.description, p.price, p.city, p.region, p.url)
+            p.is_new = False
+            session.commit()
+        except:
+            logging.error(f"Error while notifying user about product {p.olx_id} - {p.title}")
+
+
 
 if __name__ == '__main__':
     send_notifications()
