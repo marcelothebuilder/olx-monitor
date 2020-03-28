75f63c544a2e8ffc51e81dc21789d855f54d78b0
Marcelo Paixão Resende <marcelothebuilder@users.noreply.github.com>
2020-03-28 04:19:18 -0300

    reuse telegram connection instead of creating a new one for each notification

--- notification/task.py
+++ notification/task.py
@@ -1,20 +1,21 @@
 from database import Product, session
-from notification.telegram_bot import send_notification
+from notification.telegram_bot import TelegramBot
 
 import logging
 
+
 def send_notifications():
     new_products = Product.get_new()
+    telegram = TelegramBot()
 
     for product in new_products:
         p: Product = product
         try:
-            send_notification(p.image, p.title, p.description, p.price, p.city, p.region, p.url)
+            telegram.send_notification(p.image, p.title, p.description, p.price, p.city, p.region, p.url)
             p.is_new = False
             session.commit()
         except:
-            logging.error(f"Error while notifying user about product {p.olx_id} - {p.title}")
-
+            logging.exception(f"Error while notifying user about product {p.olx_id} - {p.title}")
 
 
 if __name__ == '__main__':
--- notification/telegram_bot.py
+++ notification/telegram_bot.py
@@ -10,21 +10,24 @@ PLACEHOLDER_IMG = 'https://via.placeholder.com/640x380'
 MAX_DESCRIPTION_SIZE = 500
 
 
-def send_notification(photo_url, title, description, price, city, region, url):
-    updater = Updater(token=TOKEN, use_context=True)
+class TelegramBot():
+    def __init__(self):
+        self.updater = Updater(token=TOKEN, use_context=True)
 
-    photo_url = photo_url or PLACEHOLDER_IMG
+    def send_notification(self, photo_url, title, description, price, city, region, url):
+        photo_url = photo_url or PLACEHOLDER_IMG
 
-    # truncate
-    description = description[:MAX_DESCRIPTION_SIZE] + '...' if len(description) > MAX_DESCRIPTION_SIZE else description
+        # truncate
+        description = description[:MAX_DESCRIPTION_SIZE] + '...' if len(
+            description) > MAX_DESCRIPTION_SIZE else description
 
-    markdown_message = f"""
-<strong>{title}</strong> - R$ {price}
+        markdown_message = f"""
+    <strong>{title}</strong> - R$ {price}
 
-{city} - {region}
+    {city} - {region}
 
-{description}
+    {description}
 
-<a href="{url}">{url}</a>
-    """
-    updater.bot.send_photo(chat_id=CHAT_ID, photo=photo_url, parse_mode='HTML', caption=markdown_message)
+    <a href="{url}">{url}</a>
+        """
+        self.updater.bot.send_photo(chat_id=CHAT_ID, photo=photo_url, parse_mode='HTML', caption=markdown_message)
